d3c16ea06ce3096cf18c2b4aa77d65f92c8237f7
nilay286 <nilayozel2001@gmail.com>
2022-01-02 00:15:03 +0300

    Initial commit

---
+++ app/src/main/java/com/nilayozel/dortislemkotlin/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.nilayozel.dortislemkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun topla(view : View){
+        var sayi1 = editText.text.toString().toIntOrNull()
+        var sayi2 = editText2.text.toString().toIntOrNull()
+
+        if(sayi1==null || sayi2==null){
+            textView.text="sayı giriniz"
+        }
+        else{
+            var sonuc= sayi1+sayi2
+            textView.text= "Sonuc: ${sonuc}"
+        }
+    }
+
+    fun cikar(view : View){
+        var sayi1 = editText.text.toString().toIntOrNull()
+        var sayi2 = editText2.text.toString().toIntOrNull()
+
+        if(sayi1==null || sayi2==null){
+            textView.text="sayı giriniz"
+        }
+        else{
+            var sonuc= sayi1-sayi2
+            textView.text= "Sonuc: ${sonuc}"
+        }
+    }
+
+    fun bol(view : View){
+        var sayi1 = editText.text.toString().toDoubleOrNull()
+        var sayi2 = editText2.text.toString().toDoubleOrNull()
+
+        if(sayi1==null || sayi2==null){
+            textView.text="sayı giriniz"
+        }
+        else{
+            var sonuc= sayi1/sayi2
+            textView.text= "Sonuc: ${sonuc}"
+        }
+    }
+
+    fun carp(view : View){
+        var sayi1 = editText.text.toString().toIntOrNull()
+        var sayi2 = editText2.text.toString().toIntOrNull()
+
+        if(sayi1==null || sayi2==null){
+            textView.text="sayı giriniz"
+        }
+        else{
+            var sonuc= sayi1*sayi2
+            textView.text= "Sonuc: ${sonuc}"
+        }
+    }
+
+}
